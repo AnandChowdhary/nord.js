00152664c36dc46d17d9a9ba36865728ae052b19
Anand Chowdhary <mail@anandchowdhary.com>
2022-08-11 11:32:42 +0530

    feat(app): Add dev script

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,9 +2,6 @@
 
 import { execSync } from "child_process";
 import { build } from "./build";
-import {} from "process";
-import { register } from "@swc-node/register/lib/register";
-import { compile } from "./compile";
 
 void (async (): Promise<void> => {
   const command = process.argv.pop();
@@ -12,11 +9,10 @@ void (async (): Promise<void> => {
 
   if (command === "build") return build();
   if (command === "dev") {
-    return compile();
-    // execSync(
-    //   "node --loader=node_modules/@nordjs/cli/node_modules/@swc-node/register app.ts",
-    //   { stdio: "inherit" }
-    // );
+    await build();
+    execSync("node -r './node_modules/@swc-node/register' app.ts", {
+      stdio: "inherit",
+    });
   }
 })().catch((error): never => {
   console.error(error);
