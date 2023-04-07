43fe86722eb703fb30e62c31508928093d2b0cb5
Anand Chowdhary <mail@anandchowdhary.com>
2023-04-07 19:43:31 +0200

    chore: Delete all old files

--- packages/development-node-express/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Route } from "@nordjs/types";
-import { z } from "@nordjs/validator";
-
-export const GET: Route = ({ useQuery }) => {
-  const data = useQuery(z.object({ id: z.string() }));
-  return { success: { id: data.id } };
-};
